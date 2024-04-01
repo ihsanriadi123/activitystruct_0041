b4c6ab40582c5b278b960e0f6fe5dc151155bd33
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-04-01 14:06:28 +0700

    membuat data mahasiswa dengan alamat kota dan desa

--- strukturc_2.cpp
+++ strukturc_2.cpp
@@ -6,7 +6,6 @@ struct detailalamat{
     string kota;
 };
 
-
 struct mahasiswa {
     string nim;
     string nama;
@@ -14,3 +13,25 @@ struct mahasiswa {
     int umur;
 };
 
+int main() {
+    mahasiswa mhs;
+    cout << "nomor mahasiswa: ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa: ";
+    cin >> mhs.nama;
+
+    cout << "alamat mahasiswa: ";
+    cout << "\t nama desa : "
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+    
+    cout << "umur mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim : " << mhs.nim;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat: " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+}
